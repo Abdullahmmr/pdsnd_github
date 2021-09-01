79390bafd4f3552d822e6e9f97dc19a2f40fc6b3
Abdullah <a1m98@outlook.sa>
2021-09-01 14:13:18 +0300

    remove comment from station stats

--- bikeshare.py
+++ bikeshare.py
@@ -89,7 +89,6 @@ def time_stats(df):
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
-    print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
     # display most commonly used start station
